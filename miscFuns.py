91f2abbeda4ef99b3a9d80c9bed2a325d8dd4cce
Sidney Cozier <scozier@g.hmc.edu>
2018-12-18 07:48:48 -0800

    ADSR v1.1 and miscFuns updates
    Fixed bug in ADSR where the segmentation points were continuously redefined. Changed parameter values, but I also found that the parameter values are a bug in and of themselves. In brief, the parameter values (sound threshold, steady-state slope, and dx of average slope calculation) need to be changed for each sound in order to pick up the sound's ADSR values properly. For now, we will keep one set of parameters and hope it works out ok.
    
    calcADSR and envelope functions were copied over into miscFuns so that they can be used without importing the Feature Vectors.ipynb file, which will remain a file for demos and experimentation.

--- miscFuns.py
+++ miscFuns.py
@@ -61,3 +61,96 @@ def plotWave(audio, t=None, t0=0, title='', sr=None, short=False):
     plt.xlabel('Time (s)')
     
     if short: return short_audio
+    
+
+def envelope(sound, M=20, sr=22050):
+    ''' Generates 1-sided sound envelope based on simple moving averaging method. In order to track the sound
+        onset better, the averaging window is biased towards the past data points, rather than centered
+        around the current point.
+        
+        @param sound  sound observation in time-energy domain
+        @param M      1/3 of window size of moving average (window from n-2M to n+M)
+    '''
+    if sr==None:
+        print('WARNING: Sample rate not provided to plotWave. Sample rate has defaulted to 22050 Hz.')
+        sr=22050
+    
+    #### NEW METHOD ######
+#     sound = np.array(list(map(mf.halfWave, sound)))
+    
+#     retain only the peaks (filter?)
+#     use linear interpolation between the points
+#     ignore any pre-peak valleys (may not be necessary?)
+
+#     Ask Prof. Tsai about using autocorrelation for this
+    
+    #### OLD METHOD ######
+    
+    sound = np.abs(sound)
+        
+    for frame in np.arange(len(sound)):
+        start = max(0,frame-2*M) # Bias window towards past data
+        end = min(len(sound)-1, frame+M)
+        sound[frame] = sum(sound[start:end])/(max(end-start,1)) 
+    
+    return sound
+
+
+def calcADSR(sound, env, sr=None):
+    ''' sound is the time-energy representation (raw audio data)
+        x variables are in samples, t variables are in seconds
+        
+        This function assumes that the sound has been windowed appropriately
+        such that the first index is the beginning of the sound.
+        
+        I categorized sounds into 2 categories based on the severity of release.
+        If the post-decay section is a gradual release, this counts it as 0 sustain and full release.
+        IF the post-decay section is followed by a hard release section, both sustain and release will be positive.
+    '''
+    if sr==None:
+        print('WARNING: Sample rate not provided to ADSR. Sample rate has defaulted to 22050 Hz.')
+        sr=22050
+    
+    thresh = 0.001 # Minimum sound level (envelope)
+    min_slope = 3e-6 # minimum slope threshold (for steady state vs. release)
+    slope_run = int(0.1*sr) # Step width for average slope calculation
+    
+    
+#     x_peak = argmax(sound) # Should this be used?
+    x_peak_env = np.argmax(env)
+    
+    # Initialize variables
+    x_start = 0
+    x_end = len(sound)-1
+    
+    # Look for start of sound
+    for x,eVal in enumerate(env):
+        if eVal > thresh: # Start of sound
+            x_start = x
+            break
+    
+    # Look for post-peak decay, sustain, and release.
+    x_ss1 = x_ss2 = x_peak_env
+    inDecay = True
+    for x,eVal in enumerate(env[x_peak_env:]):
+        x += x_peak_env
+        slope = (env[min(x+slope_run, x_end)]-env[x])/(min(x+slope_run, x_end) - x)
+#         print([x/sr+t[0],slope])
+
+        if (eVal < thresh) or (x == x_end-1): # End of sound
+            x_end = x # Minor off-by-one error?           
+            break
+            
+        elif inDecay and (np.abs(slope) < min_slope): # Start of a sustain/gradual release section
+            x_ss1 = x_ss2 = x # Initialize x_ss2 in case of no hard release
+            inDecay = False
+        elif (inDecay==False) and (np.abs(slope) >= min_slope): # Start of a hard release
+            x_ss2 = x
+            
+    #print('x_start: {}, x_peak_env: {}, x_ss1: {}, x_ss2: {}, x_end: {}'.format(x_start, x_peak_env, x_ss1, x_ss2, x_end))
+    attack = (x_peak_env-x_start)/sr
+    decay = (x_ss1-x_peak_env)/sr 
+    sustain = (x_ss2-x_ss1)/sr 
+    release = (x_end-1-x_ss2)/sr
+    
+    return attack, decay, sustain, release
